67d329ce7380e8fd4064ecc4503960bcf5f8086e
Jonru22 <jon.ruizh@alumni.mondragon.edu>
2025-01-27 11:50:55 +0100

    Update point.cpp

--- src/point.cpp
+++ src/point.cpp
@@ -1,4 +1,16 @@
 #include "point.h"
-void freePoints(point_t** points) {
-	//TODO 2. Free the memory allocated for the point_t array
+#include <stdlib.h>
+
+// Frees the memory allocated for points
+void freePoints(point_t** const points) {
+    // Check if the pointer to points and the points themselves are not null
+    if (points && *points) {
+        // Free the allocated memory
+        free(*points);
+        // Set the pointer to null to avoid dangling pointers
+        *points = NULL;
+    }
 }
+
+
+// Frees the memory allocated for points
